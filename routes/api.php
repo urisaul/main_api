c63345fbeee0232796a50e1fbd3e75db86cf9206
uri saul <urisaul36@gmail.com>
2022-11-05 22:19:12 +0200

    small update

--- routes/api.php
+++ routes/api.php
@@ -28,14 +28,13 @@ Route::post('/login', [UserController::class, "login"]);
 
 
 
-Route::middleware(['auth:sanctum'])->group(function ()
-{
+Route::middleware(['auth:sanctum'])->group(function () {
     // user routes
     Route::post('/create-user', [UserController::class, "create"]);
     Route::get('/show-user/{id}', [UserController::class, "show"]);
     Route::patch('/update-user/{id}', [UserController::class, "update"]);
     Route::delete('/delete-user/{id}', [UserController::class, "delete"]);
-    
+
     // scrape section
     Route::get('/scrape_url', [ScrapeController::class, "scrape_url"]);
 
@@ -46,7 +45,7 @@ Route::middleware(['auth:sanctum'])->group(function ()
     Route::post('/recipes/add', [RecipeController::class, "Add"]);
     Route::patch('/recipes/update', [RecipeController::class, "Update"]);
     Route::delete('/recipes/delete', [RecipeController::class, "Delete"]);
-    
+
     // Comment routes
     Route::get('/comments/all', [CommentController::class, "GetAll"]);
     Route::get('/comments/for_recipe/{recipe_id}', [CommentController::class, "get_for_recipe"]);
@@ -56,9 +55,12 @@ Route::middleware(['auth:sanctum'])->group(function ()
 
 
     // test routes
-    Route::get('/testing', function (Request $request)
-    {
-        Mail::to($request->user()['email'])->send(new GeneralEmail());
+    Route::get('/testing', function (Request $request) {
+        $data = [
+            "subject" => "this is a test email", 
+            "body" => "this is a test body of the email!!"
+        ];
+        Mail::to($request->user()['email'])->send(new GeneralEmail($data));
 
         return [
             "message" => "You have reached your destination!!",
@@ -72,19 +74,19 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
- 
+
 // reset password routes
 
 Route::post('/forgot-password', function (Request $request) {
     $request->validate(['email' => 'required|email']);
- 
+
     $status = Password::sendResetLink(
         $request->only('email')
     );
- 
+
     return $status === Password::RESET_LINK_SENT
-                ? back()->with(['status' => __($status)])
-                : back()->withErrors(['email' => __($status)]);
+        ? back()->with(['status' => __($status)])
+        : back()->withErrors(['email' => __($status)]);
 })->middleware('guest')->name('password.email');
 
 Route::get('/reset-password/{token}', function ($token) {
