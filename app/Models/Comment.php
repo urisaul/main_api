2c95e7a4860ab7b98bbacc54a9874fd83b6c487c
uri saul <urisaul36@gmail.com>
2022-11-08 16:30:18 +0200

    added second db connection

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,8 @@ class Comment extends Model
 {
     use HasFactory;
 
+    protected $connection = 'mysql2';
+
     const CREATED_AT = 'date_created';
     const UPDATED_AT = 'date_updated';
 
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+    
+    protected $connection = 'mysql2';
+    protected $table = 'recipes_1';
 
     const CREATED_AT = 'date_created';
     const UPDATED_AT = 'date_updated';
-
-    protected $table = 'recipes_1';
 
     protected $fillable = ['title', 'auth_id', 'recipe_kind', 'description', 'ingredients', 'body', 'categories', 'shared_with'];
 
