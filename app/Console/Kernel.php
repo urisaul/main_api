437f78d5ba2c081b91855e409539697c338b7c5d
uri saul <urisaul36@gmail.com>
2023-08-27 20:33:29 +0300

    added schedule test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Http\Controllers\TaskController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -19,6 +20,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         $schedule->call(function(){
+            Log::info("Testing the schedule");
             return TaskController::run_tasks();
         })->everyMinute();
 
