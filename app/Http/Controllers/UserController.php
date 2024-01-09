1a1ca70c5a47ee2ed2bb057c983993cbbcbfaa80
uri saul <urisaul36@gmail.com>
2024-01-09 17:31:43 +0200

    update for user ctrlr

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Libraries\Utils;
 use App\Mail\GeneralEmail;
+use App\Models\QUser;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\ValidationException;
@@ -32,16 +33,16 @@ class UserController extends Controller
         return $user;
     }
 
-    public function update (Request $request, $id)
-    {
-        return User::where('id', $id)
-            ->update($request->all());
-    }
+    // public function update (Request $request, $id)
+    // {
+    //     return User::where('id', $id)
+    //         ->update($request->all());
+    // }
 
-    public function delete ($id, $model)
-    {
-        return $user = User::destroy($id);
-    }
+    // public function delete ($id, $model)
+    // {
+    //     return $user = User::destroy($id);
+    // }
 
 
 
@@ -84,5 +85,30 @@ class UserController extends Controller
         // $user = User::find(8);
         // return $user->createToken($request->token_name?? "main_tok", ['server:update']);
         // return true;
+    }
+
+
+    public function q_login (Request $request)
+    {
+        $request->validate([
+            'email'       => 'required|email',
+            'password'    => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = QUser::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken($request->device_name, ['create', 'update', 'delete']);
+
+        return [
+            "success" => true,
+            "token" => $token->plainTextToken,
+        ];
     }
 }
