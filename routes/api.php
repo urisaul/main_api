5d83137a646f55624c18cdc40d7252d034cb63d6
uri saul <urisaul36@gmail.com>
2024-01-12 11:20:55 +0200

    update for Quser sys

--- routes/api.php
+++ routes/api.php
@@ -36,49 +36,49 @@ Route::post("/parsha/send_email_req", [ParshaController::class, "send_email_req"
 Route::post("/parsha/schedule_send", [ParshaController::class, "schedule_send_req"]);
 
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    // user routes
-    // Route::post('/create-user', [UserController::class, "create"]);
-    Route::get('/show-user/{id}', [UserController::class, "show"]);
-    Route::patch('/update-user/{id}', [UserController::class, "update"]);
-    Route::delete('/delete-user/{id}', [UserController::class, "delete"]);
+// Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+//     // user routes
+//     // Route::post('/create-user', [UserController::class, "create"]);
+//     Route::get('/show-user/{id}', [UserController::class, "show"]);
+//     Route::patch('/update-user/{id}', [UserController::class, "update"]);
+//     Route::delete('/delete-user/{id}', [UserController::class, "delete"]);
 
-    // scrape section
-    Route::get('/scrape_url', [ScrapeController::class, "scrape_url"]);
-
-
-    // Recipe routes
-    Route::get('/recipes/all', [RecipeController::class, "GetAll"]);
-    Route::get('/recipes/allwcomments', [RecipeController::class, "GetAllWithComments"]);
-    Route::get('/recipes/single/{id}', [RecipeController::class, "GetSingle"]);
-    Route::post('/recipes/add', [RecipeController::class, "Add"]);
-    Route::patch('/recipes/update', [RecipeController::class, "Update"]);
-    Route::delete('/recipes/delete', [RecipeController::class, "Delete"]);
-
-    // Comment routes
-    Route::get('/comments/all', [CommentController::class, "GetAll"]);
-    Route::get('/comments/for_recipe/{recipe_id}', [CommentController::class, "get_for_recipe"]);
-    Route::post('/comments/add', [CommentController::class, "Add"]);
-    Route::patch('/comments/update', [CommentController::class, "Update"]);
-    Route::delete('/comments/delete', [CommentController::class, "Delete"]);
+//     // scrape section
+//     Route::get('/scrape_url', [ScrapeController::class, "scrape_url"]);
 
 
-    // test routes
-    Route::get('/testing', function (Request $request) {
-        $data = [
-            "subject" => "this is a test email",
-            "body" => "this is a test body of the email!!"
-        ];
-        // Mail::to($request->user()['email'])->send(new GeneralEmail($data));
+//     // Recipe routes
+//     Route::get('/recipes/all', [RecipeController::class, "GetAll"]);
+//     Route::get('/recipes/allwcomments', [RecipeController::class, "GetAllWithComments"]);
+//     Route::get('/recipes/single/{id}', [RecipeController::class, "GetSingle"]);
+//     Route::post('/recipes/add', [RecipeController::class, "Add"]);
+//     Route::patch('/recipes/update', [RecipeController::class, "Update"]);
+//     Route::delete('/recipes/delete', [RecipeController::class, "Delete"]);
 
-        return [
-            "message" => "You have reached your destination!!",
-            "user"    => $request->user(),
-        ];
-    });
-});
+//     // Comment routes
+//     Route::get('/comments/all', [CommentController::class, "GetAll"]);
+//     Route::get('/comments/for_recipe/{recipe_id}', [CommentController::class, "get_for_recipe"]);
+//     Route::post('/comments/add', [CommentController::class, "Add"]);
+//     Route::patch('/comments/update', [CommentController::class, "Update"]);
+//     Route::delete('/comments/delete', [CommentController::class, "Delete"]);
 
-Route::middleware(['auth:sanctum', /*'verified',*/ 'client'])->group(function () {
+
+//     // test routes
+//     Route::get('/testing', function (Request $request) {
+//         $data = [
+//             "subject" => "this is a test email",
+//             "body" => "this is a test body of the email!!"
+//         ];
+//         // Mail::to($request->user()['email'])->send(new GeneralEmail($data));
+
+//         return [
+//             "message" => "You have reached your destination!!",
+//             "user"    => $request->user(),
+//         ];
+//     });
+// });
+
+Route::middleware(['auth:sanctum', 'verified', 'client'])->group(function () {
 
     Route::get('a_v1/{client}/{object_id}/get', [GenManagerController::class, "get_"]);
     Route::get('a_v1/{client}/{object_id}/get/{id}', [GenManagerController::class, "get_one_"]);
